c1cb75150c1d59bf71ccea5ded848b3aeeaa227a
yedio <yediobjet@gmail.com>
2023-05-10 17:32:04 +0900

    map: add service (search addresss)

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -7,14 +7,13 @@ declare global {
 }
 
 export default function Map() {
-  const locationX = 33.450701;
-  const locationY = 126.570667;
+  const address = '서울 종로구 세종로 1-68';
 
   useEffect(() => {
     const mapScript = document.createElement('script');
 
     mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services&autoload=false`;
 
     document.head.appendChild(mapScript);
 
@@ -22,7 +21,7 @@ export default function Map() {
       window.kakao.maps.load(() => {
         const mapContainer = document.getElementById('map');
         const mapOption = {
-          center: new window.kakao.maps.LatLng(locationX, locationY), // 지도의 중심좌표
+          center: new window.kakao.maps.LatLng(0, 0), // 지도의 중심좌표 (경도 & 위도)
           level: 3, // 지도의 확대 레벨
         };
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
@@ -38,17 +37,27 @@ export default function Map() {
         const zoomControl = new window.kakao.maps.ZoomControl();
         map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
-        // 마커 표시
-        const markerPosition = new window.kakao.maps.LatLng(
-          locationX,
-          locationY
-        );
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
+        // 주소로 좌표 검색 및 마커 표시
+        const geocoder = new window.kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, function (result: any, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const coords = new window.kakao.maps.LatLng(
+              result[0].y,
+              result[0].x
+            );
+            map.setCenter(coords);
+
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
+            marker.setMap(map);
+          }
         });
-        marker.setMap(map);
       });
     };
+
     mapScript.addEventListener('load', onLoadKakaoMap);
   }, []);
 
