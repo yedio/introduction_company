0bd40f23aefa10edef9dc4a6078d046db34892ca
yedio <yediobjet@gmail.com>
2023-05-10 15:56:26 +0900

    map: load kakaomap

--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,14 +1,33 @@
 import Script from 'next/script';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 export default function Map() {
-  useEffect(() => {}, []);
-  return (
-    <>
-      <Script
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}`}
-      />
-      <div id="map" className="w-[500px] h-[400px] bg-grey-40"></div>
-    </>
-  );
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+  }, []);
+
+  return <div id="map" className="w-[500px] h-[400px] bg-grey-40"></div>;
 }
