d25fb396079ff35a66016f24662169c760df5569
yedio <82350743+yedio@users.noreply.github.com>
2023-05-10 17:36:33 +0900

    Feature/map: kakao map 구현 (#1)
    * map: add kakao sdk
    
    * map: load kakaomap
    
    * map: add option (mapType & zoomControl & marker)
    
    * map: add service (search addresss)
    
    * map: refactor

---
+++ src/components/common/Map.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+export default function Map() {
+  const address = '서울 종로구 세종로 1-68';
+
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_KEY}&libraries=services&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(0, 0), // 지도의 중심좌표 (경도 & 위도)
+          level: 3, // 지도의 확대 레벨
+        };
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        // 지도 & 스카이뷰 옵션
+        const mapTypeControl = new window.kakao.maps.MapTypeControl();
+        map.addControl(
+          mapTypeControl,
+          window.kakao.maps.ControlPosition.TOPRIGHT
+        );
+
+        // 줌 옵션
+        const zoomControl = new window.kakao.maps.ZoomControl();
+        map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
+        // 주소로 좌표 검색 및 마커 표시
+        const geocoder = new window.kakao.maps.services.Geocoder();
+
+        geocoder.addressSearch(address, function (result: any, status: any) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const coords = new window.kakao.maps.LatLng(
+              result[0].y,
+              result[0].x
+            );
+            map.setCenter(coords);
+
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
+            marker.setMap(map);
+          }
+        });
+      });
+    };
+
+    mapScript.addEventListener('load', onLoadKakaoMap);
+  }, []);
+
+  return <div id="map" className="w-[500px] h-[400px] bg-grey-40" />;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head';
+import CompanyLocation from '../components/pages/main/CompanyLocation';
 
 export default function Home() {
   return (
     <>
       <h1 className=" font-bold underline">Hello world! / 안녕하세요?</h1>
+      <CompanyLocation />
     </>
   );
 }
